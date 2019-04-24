bfb272759ec53c3bbf097ee5214ec975cd946ab0
Felipe Mesquita <lipemesq@gmail.com>
2019-04-24 09:08:59 -0300

    lhjkl

--- Experimental de got/ViewController.swift
+++ Experimental de got/ViewController.swift
@@ -14,6 +14,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("COISAS ADICIONAIS")
+        
+        print("Isso deveria existir?")
+        
+        print("ALGO deveria existir??")
     }
 
 
